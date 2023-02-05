7c7cc9e929649e4aac105e329d8a2cd4ae5ea47a
Léo Grambert <leogrambert@protonmail.com>
2023-02-05 15:54:53 +0100

    feat: add approveRequest and finaliazeRequest

--- Campaign/Campaign.sol
+++ Campaign/Campaign.sol
@@ -8,26 +8,30 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     address public manager;
     uint256 public minimumContribution;
-    address[] public approvers;
     Request[] public requests;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    constructor (uint256 minimum) public {
+    function Campaign(uint256 minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(string description, uint value, address recipient) public restricted {
@@ -35,8 +39,26 @@ contract Campaign {
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0
         });
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        require(approvers[msg.sender]);
+        Request storage request = requests[index];
+        require(!request.approvals[msg.sender]);
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(!request.complete);
+        require (request.approvalCount > (approversCount / 2));
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
