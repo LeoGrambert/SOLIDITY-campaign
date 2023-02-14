b41d54d054b4c46bd50f76986d20089ddd6ec0e1
Léo Grambert <leogrambert@protonmail.com>
2023-02-14 20:52:58 +0100

    feat: start project and test

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,6 +1,17 @@
-// SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
 
 contract Campaign {
     struct Request {
@@ -12,9 +23,9 @@ contract Campaign {
         mapping(address => bool) approvals;
     }
 
-    address public manager;
-    uint256 public minimumContribution;
     Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
     mapping(address => bool) public approvers;
     uint public approversCount;
 
@@ -23,40 +34,45 @@ contract Campaign {
         _;
     }
 
-    function Campaign(uint256 minimum) public {
-        manager = msg.sender;
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
+
         approvers[msg.sender] = true;
         approversCount++;
     }
 
     function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
-            description: description,
-            value: value,
-            recipient: recipient,
-            complete: false,
-            approvalCount: 0
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
         });
+
         requests.push(newRequest);
     }
 
     function approveRequest(uint index) public {
-        require(approvers[msg.sender]);
         Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
+
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
 
     function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
-        require (request.approvalCount > (approversCount / 2));
 
         request.recipient.transfer(request.value);
         request.complete = true;
