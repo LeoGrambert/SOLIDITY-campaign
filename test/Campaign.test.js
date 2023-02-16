8290ce002533f0338bef8c7fa0574b2c26ef8553
Léo Grambert <leogrambert@protonmail.com>
2023-02-16 21:33:52 +0100

    feat: add some tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -6,6 +6,8 @@ const web3 = new Web3(ganache.provider());
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require("../ethereum/build/Campaign.json");
 
+const description = "Buy batteries";
+
 let accounts;
 let factory;
 let campaignAddress;
@@ -34,5 +36,67 @@ describe('Campaigns', () => {
     it('should deploy a factory and a campaign', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    });
+
+    it('should mark caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('should allow people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1],
+        });
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
+    });
+
+    it('should require a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '5',
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('should allow a manager to make a payment request', async () => {
+        await campaign.methods
+            .createRequest(description, '100', accounts[1])
+            .send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+        const request = await campaign.methods.requests(0).call();
+        assert.equal(description, request.description);
+    });
+
+    it('should process requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        });
+        await campaign.methods
+            .createRequest(description, web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        const balanceInWei = await web3.eth.getBalance(accounts[1]);
+        const balanceInEther = parseFloat(web3.utils.fromWei(balanceInWei, "ether"));
+        assert(balanceInEther > 104.9); // .1 difference for gas fee
     });
 });
